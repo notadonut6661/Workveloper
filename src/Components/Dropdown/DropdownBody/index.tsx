e1f855b0ab8b47c86876303507daf3bbd5ab5026
notadonut6661 <wotfan1989@gmail.com>
2022-12-17 03:33:05 +0200

    Dropdown menu in progress

--- src/Components/Dropdown/DropdownBody/index.tsx
+++ src/Components/Dropdown/DropdownBody/index.tsx
@@ -2,10 +2,12 @@ import './style.scss';
 export default function DropdownBody(): JSX.Element {
     return (<div className={'DropdownBody Find'}>
         <ul>
-            <li><a href={'Employee'}>Looking for new qualified employees</a></li>
+            <li id={'Employee'}><a href={'Employee'}>Looking for new qualified employees</a></li>
+            <li id={'Worker'}><a href={'Employee'}>Need a project to work on</a></li>
+
         </ul>
         <div className={'Image'}>
-            {/*<img src={}/>*/}
+            <div></div>
         </div>
     </div>);
 }
